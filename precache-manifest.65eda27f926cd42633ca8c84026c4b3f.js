94379d78d3f7b24c7b635730370f604b7b5b1cc7
Vitaly Rudenko <maurice.dederick@gmail.com>
2019-08-12 20:50:45 +0300

    Updates

---
+++ precache-manifest.65eda27f926cd42633ca8c84026c4b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f1307a467d08c0918c4532ae3f1c76",
+    "url": "/json-editor/index.html"
+  },
+  {
+    "revision": "8b19906fb80da7b0ffcb",
+    "url": "/json-editor/static/css/main.3830d130.chunk.css"
+  },
+  {
+    "revision": "5bd5fe70b74d9ee5f509",
+    "url": "/json-editor/static/js/2.43923ec7.chunk.js"
+  },
+  {
+    "revision": "8b19906fb80da7b0ffcb",
+    "url": "/json-editor/static/js/main.ec1a41a7.chunk.js"
+  },
+  {
+    "revision": "94e5e51df9d3db8ec6ab",
+    "url": "/json-editor/static/js/runtime~main.7c3b5260.js"
+  }
+]);
