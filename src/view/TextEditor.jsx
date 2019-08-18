2ae03750e4b4adf86baf5ff9f2a0ccaae1d9c0d7
Vitaly Rudenko <maurice.dederick@gmail.com>
2019-08-18 17:06:16 +0300

    Added Ctrl+S hotkey to save changes

--- src/view/TextEditor.jsx
+++ src/view/TextEditor.jsx
@@ -3,12 +3,27 @@ import './TextEditor.css';
 
 export const TextEditor = ({ value: initialValue, onChange, className = '' }) => {
     const [value, setValue] = useState(initialValue);
-    const [isFocused, setIsFocused] = useState(true);
+    const [isFocused, setIsFocused] = useState(false);
     const revert = useCallback(() => setValue(initialValue), [initialValue]);
 
     useEffect(() => {
         setValue(initialValue);
     }, [initialValue]);
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (event.ctrlKey && event.key === 's') {
+                event.preventDefault();
+
+                if (value !== initialValue) {
+                    onChange(value, revert);
+                }
+            }
+        };
+
+        window.addEventListener('keydown', handler);
+        return () => window.removeEventListener('keydown', handler);
+    }, [initialValue, value, onChange, revert]);
 
     const handleChange = useCallback((event) => {
         const value = event.target.value;
@@ -30,6 +45,22 @@ export const TextEditor = ({ value: initialValue, onChange, className = '' }) =>
         }
     }, [initialValue, onChange, revert]);
 
+    const handleKeyDown = useCallback(/** @param {KeyboardEvent} event */ (event) => {
+        const target = event.target;
+        const value = target.value;
+
+        if (event.key === 'Tab') {
+            event.preventDefault();
+
+            const identation = '    ';
+            const start = target.selectionStart;
+            const end = target.selectionEnd;
+
+            target.value = value.substring(0, start) + identation + value.substring(end);
+            target.selectionEnd = start + identation.length; 
+        }
+    }, []);
+
     return (
         <div className={`text-editor ${className}`}>
             <textarea
@@ -38,12 +69,13 @@ export const TextEditor = ({ value: initialValue, onChange, className = '' }) =>
                 onBlur={handleBlur}
                 onFocus={handleFocus}
                 value={value}
+                onKeyDown={handleKeyDown}
                 autoCapitalize="off"
                 autoCorrect="off"
                 autoComplete="off"
                 spellCheck="false"
             />
-            {isFocused && <div className="text-editor__apply-notice">Click outside to apply changes</div>}
+            {isFocused && <div className="text-editor__apply-notice">Click outside to apply changes (Ctrl+S)</div>}
         </div>
     )
 };
